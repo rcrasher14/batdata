b17d3c38f49879e6b1de870b0a2ca9f21de714ce
rcrasher14 <rcrasher14@gmail.com>
2017-02-14 21:18:45 -0500

    Handling NAs and the no-1s case, commenting

--- distance_to_one.R
+++ distance_to_one.R
@@ -5,13 +5,14 @@
 # and that you've set up the za matrix and the species_tables list
 # from the 'multi_dim_data.R' script.
 
+# It maintains NAs and date data from raw data.
+# If the last observation is not a 1, then the algorithm acts as if the
+# next night after the instance would have been a 1, and calculates the distance
+# to it, such that an instance with all zeros would still have a distance value
+# for each night.
+
 ### Example for one species:
 laci_dist <- list_distance_to_one(species_tables$laci)
 
 ### A list across all species:
 all_dist_to_one <- lapply(species_tables,list_distance_to_one)
-
-
-# Current issues:
-# 1) how to keep julian calendar data fo reach item?
-# 2) how to handle NA data?  or what about zeros at the tail of an instance?
--- vtech_functions.R
+++ vtech_functions.R
@@ -40,25 +40,36 @@ list_zero_lengths <- function(x){
 # Function to generate vector with distance to next one.
 # Input: vector
 # Output: vector with distance to next ones
-distance_to_one <- function(input){
-  #input <- as.numeric(na.omit(input))
-  input_ones <- which(input==1)
+distance_to_one <- function(x){
+  input <- x[!is.na(x)]  # calculate from instance nights only
+  input_ones <- which(input==1) # Find 1's--vectors with only 0's are handled later
+  # Add length so that we have a vector with the indices of each one, and the last index
   input_ones <- c(input_ones,length(input))
-  runs <- c(input_ones[1],diff(input_ones))
+  # Now create a vector with the distance between ones(and the final index if != 1)
+  if(length(input_ones)!=0){ # if there is a 1, then use them
+    runs <- c(input_ones[1],diff(input_ones))
+  } else{ # handle the case where there are no 1's and simply run to the end
+    runs <- input_ones
+  }
+  # increment the final index to just outside the dataset
+  # so that the final diff value is 1 not zero--as if the next value would have equaled 1
   input_ones[length(input_ones)] <- input_ones[length(input_ones)] + 1
+  # now create a vector mirroring input, but with the index of the next one
+  # or the last index, whatever is sooner.
   d <- rep(input_ones,runs)
+  # fabricate a set of indices
   ind <- c(1:length(input))
-  dist_to_one <- d - ind
-  return(dist_to_one)
+  dist_to_one <- d - ind # Calculate the distance to ones
+  x[!is.na(x)]<-dist_to_one # replace non-NA values in x with dist_to_one
+  return(x)
 }
 
 # Function to iterate over a list, returning distance to next ones
 # Input: list
-# Output: list of distance to one vectors
+# Output: dataframe of distance to one vectors
 list_distance_to_one <- function(x){
-  ldist <- lapply(x,distance_to_one)
-  # Need to figure out how to maintain the date data found in the input
-  # row.names.  Trying sapply with use.names=T
-  #ldist <- sapply(x,distance_to_one,simplify=F,USE.NAMES = T)
-  return(ldist)
+  ldist <- lapply(x,distance_to_one) # Loop over list elements
+  rtn <- as.data.frame(ldist) # Coerce into a dataframe
+  row.names(rtn)<-dates # reapply date data
+  return(rtn)
 }
